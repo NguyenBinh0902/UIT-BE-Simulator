31ff9e553523839eb083d13b330c98c016fb83ac
nguyenbinh09 <21521878@gm.uit.edu.vn>
2024-12-16 20:28:40 +0700

    first commit

---
+++ src/main/java/com/example/uit_simulator/UitSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.uit_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UitSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UitSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/uit_simulator/UitSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uit_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UitSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
